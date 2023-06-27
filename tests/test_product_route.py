79314e8ec8accdf2fb6ad2d15c049f21fff6f48a
Rusiru Dilshan <kgdrusiru@gmail.com>
2023-06-27 11:30:15 +0100

    get product by id endpoint with its unit tests

--- tests/test_product_route.py
+++ tests/test_product_route.py
@@ -4,4 +4,16 @@ def test_list_products():
     response = client.get("/product/list")
     assert response.status_code == 200
     assert len(response.json()['products']) > 1
+
+def test_get_by_id():
+    response = client.get("/product/2")
+    assert response.status_code == 200
+    assert response.json() == {
+        "product" : {"product_id":2,"name":"Seasalt white shirt","category":"tops","price":15.0}
+    }
+
+def test_get_by_id_404():
+    response = client.get("/product/200")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Product not found"}
        
