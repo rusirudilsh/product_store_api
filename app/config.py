38544fadfea95f841d5b5c9317bd755600a26496
Rusiru Dilshan <kgdrusiru@gmail.com>
2023-06-29 13:18:09 +0100

    config settings

--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
 from functools import lru_cache
 from pydantic import BaseSettings
+from dotenv import dotenv_values
+
+config = dotenv_values("app/.env")
 
 class Settings(BaseSettings):
-    app_name: str
-    env_name: str
-    base_url: str
-    origins = list[str]
+    app_name: str = config.get("APP_NAME")
+    app_version: str = config.get("APP_VERSION")
+    env_name: str = config.get("ENV_NAME")
+    base_url: str = config.get("BASE_URL")
+    origins : list[str] = config.get("ALLOWED_ORIGINS").split(",")
 
-    class Config:
-        env_file = ".env"
-
-    @lru_cache()
-    def get_settings():
-        return Settings()
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
